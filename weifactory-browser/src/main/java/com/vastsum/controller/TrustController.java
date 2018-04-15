bb9e921ad42a091fbdb2861b9ea6a72ac1031249
shutu008 <1754299360@qq.com>
2018-04-15 13:29:28 +0800

    a

---
+++ weifactory-browser/src/main/java/com/vastsum/controller/TrustController.java
@@ -0,0 +1,18 @@
+package com.vastsum.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+/**
+ * 托管管理
+* @author shensj 
+* @date 2018年3月14日 下午9:47:01
+ */
+@RestController
+@RequestMapping(value = "/trust")
+@Api(value = "/trust",tags={"设备托管管理"})
+public class TrustController {
+
+}
