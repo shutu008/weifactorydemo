7a6d5a35dfdff4f99013646374c462eaa23fb692
shutu008 <1754299360@qq.com>
2018-04-24 19:52:15 +0800

    update .gitignore

--- weifactory-communication/src/main/java/com/vastsum/utils/NettyChannelMap.java
+++ weifactory-communication/src/main/java/com/vastsum/utils/NettyChannelMap.java
@@ -8,14 +8,20 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author ssj
  * @create 2017-10-19 21:22
  */
 public class NettyChannelMap implements Serializable {
     private static final long serialVersionUID = 5945996498161834927L;
-    private static Map<String,SocketChannel> map=new ConcurrentHashMap<String, SocketChannel>();
-    private static Map<SocketChannel, String> mapReverse=new ConcurrentHashMap<SocketChannel, String>();
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(NettyChannelMap.class);
+    private static final Map<String,SocketChannel> map=new ConcurrentHashMap<String, SocketChannel>();
+    private static final Map<SocketChannel, String> mapReverse=new ConcurrentHashMap<SocketChannel, String>();
     
     /**
      * 添加sn对应的channel
@@ -23,15 +29,16 @@ public class NettyChannelMap implements Serializable {
      * @param socketChannel
      */
     public static void add(String sn,SocketChannel socketChannel){
-		
+    	LOGGER.info("设备序列号为："+sn +";socketChanndel为："+ReflectionToStringBuilder.toString(socketChannel));
+    	
 			map.put(sn,socketChannel);
 			mapReverse.put(socketChannel, sn);
-		
+		LOGGER.info("已将设备序列号和通道添加到map中");
 		
     }
 
     public static Channel get(String sn){
-    	System.out.println("序列号为："+sn);
+    	LOGGER.info("通过设备序列号获取通道"+sn);
         return map.get(sn);
     }
 
