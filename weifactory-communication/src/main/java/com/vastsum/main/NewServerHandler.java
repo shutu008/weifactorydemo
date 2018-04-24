7677518449dfb0d06a923b9095a62e8653e99729
zhaoming0018 <zhaoming0018@126.com>
2018-04-24 18:42:15 +0800

    test

--- weifactory-communication/src/main/java/com/vastsum/main/NewServerHandler.java
+++ weifactory-communication/src/main/java/com/vastsum/main/NewServerHandler.java
@@ -3,7 +3,6 @@ package com.vastsum.main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.vastsum.entity.CommunicationLog;
 import com.vastsum.entity.CommunicationMessage;
